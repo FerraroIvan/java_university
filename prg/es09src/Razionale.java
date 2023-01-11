207788135829cfccd471c1594319d4ce38831ea9
Ivan Ferraro <ivan@shadowos.fritz.box>
2023-01-11 20:55:08 +0100

    commit

--- prg/es09src/Razionale.java
+++ prg/es09src/Razionale.java
@@ -1,52 +1,46 @@
 package prg.es09;
 
-public class Razionale extends Numero{
-    private int num, den;
+public class Razionale extends Numero {
+  private int num, den;
 
-    public Razionale(int num, int den){
-        setNum(num);
-        setDen(den);
+  public Razionale(int num, int den) {
+    setNum(num);
+    setDen(den);
+  }
+
+  public int getNum() { return num; }
+
+  public int getDen() { return den; }
+
+  public void setNum(int num) { this.num = num; }
+
+  public void setDen(int den) {
+    if (den != 0)
+      this.den = den;
+    else
+      throw new ArithmeticException("Non puoi mettere zero a denominatore");
+  }
+
+  public Razionale somma(Numero r) {
+    int n = 0, d = 0;
+    if (r instanceof Razionale) {
+      Razionale t = (Razionale)r;
+      d = this.getDen() * t.getDen();
+      n = (this.getNum() * t.getNum()) + (t.getNum() * this.getDen());
+      return semplifica(n, d);
     }
+    return null;
+  }
 
-    public int getNum(){
-        return num;
+  public Razionale sottrai(Numero r) {
+    int n = 0, d = 0;
+    if (r instanceof Razionale) {
+      Razionale t = (Razionale)r;
     }
+  }
 
-    public int getDen(){
-        return den;
-    }
-
-    public void setNum(int num){
-        this.num = num;
-    }
-
-    public void setDen(int den){
-        if(den != 0) this.den = den;
-        else throw new ArithmeticException("Non puoi mettere zero a denominatore");
-    }
-
-    public Razionale somma(Numero r){
-        int n = 0, d = 0;
-        if(r instanceof Razionale){
-            Razionale t = (Razionale) r;
-            d = this.getDen()*t.getDen();
-            n = (this.getNum()*t.getNum()) + (t.getNum()*this.getDen());
-            return semplifica(n, d);
-        }
-        return null;
-    }
-
-    public Razionale sottrai(Numero r){
-        int n = 0, d = 0;
-        if(r instanceof Razionale){
-            Razionale t = (Razionale) r;
-        }
-    }
-
-    public void moltiplica(Razionale r){
-        int num = this.getNum() * r.getNum();
-        int den =
-    }
-
-
+  public void moltiplica(Razionale r) {
+    int num = this.getNum() * r.getNum();
+    int den =
+  }
 }
